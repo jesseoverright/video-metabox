00e9281320c8c64364b51f7abcf1b4fc24ff5d0d
Jesse Overright <jesseoverright@gmail.com>
2013-10-08 16:42:52 -0500

    fixes #1 removed storing external thumbnails

--- video-metabox.php
+++ video-metabox.php
@@ -3,7 +3,7 @@
  * Plugin Name: Video Metabox
  * Plugin URI: https://github.com/jesseoverright/video-metabox
  * Description: Adds a video metabox plugin to your site.
- * Version: 0.3 beta
+ * Version: 1.0
  * Author: Jesse Overright
  * Author URI: http://about.me/joverright
  * License: GPL2
@@ -32,7 +32,7 @@ add_action('init', 'add_video_metabox_css');
 
 function add_video_metabox () {
      add_meta_box( 'video-metabox', 'Video', 'video_metabox', 'post', 'normal', 'high');
-     wp_enqueue_style( 'video-metabox-admin-css', plugins_url( 'video-metabox-admin.css', __FILE__) );
+     wp_enqueue_style( 'video-metabox-css', plugins_url( 'video-metabox.css', __FILE__) );
 }
 
 function add_video_metabox_css() {
@@ -47,11 +47,9 @@ function video_metabox () {
     
     $video_id = get_post_meta($post->ID, 'video_id', true);
     $video_type = get_post_meta($post->ID, 'video_type', true);
-    $video_thumb_url = get_post_meta($post->ID, 'video_thumb_url', true);
-    
-    if ($video_thumb_url != "")
-    {
-        echo "<img src=\"{$video_thumb_url}\" /><br />";    
+
+    if ($video_id != '' && $video_type != '') {
+        render_video($video_id, $video_type);
     }
     ?>
     <label>Video Type:
@@ -62,7 +60,6 @@ function video_metabox () {
     <label>Video ID:
     <input type="text" name="video_id" value="<?php echo $video_id; ?>" size="20" /></label>
     <p>Example Video IDs: www.youtube.com/watch?v=<b>hfbwoCqLgJo</b>, http://vimeo.com/<b>29491315</b></p>
-
     <?php
 }
     
@@ -95,56 +92,9 @@ function save_video_metabox( $post_id ) {
     $video_thumb_url = get_post_meta($post_id, 'video_thumb_url', true);
     $video_id = get_post_meta($post_id, 'video_id', true);
     $video_type = get_post_meta($post_id, 'video_type', true);
-            
-    // depending on the video type, get image location
-    $video_thumb_url = get_video_thumb($video_id, $video_type);
-
-    // if no video, remove thumbnail
-    if ($video_id == '')
-    {
-        $video_thumb_url = '';
-    }
-    
-    if(get_post_meta($post_id, 'video_thumb_url') == '')
-    add_post_meta($post_id, 'video_thumb_url', $video_thumb_url, true);
-    elseif($video_thumb_url != get_post_meta($post_id, 'video_thumb_url', true))
-    update_post_meta($post_id, 'video_thumb_url', $video_thumb_url);
-    elseif($video_thumb_url == '')
-    delete_post_meta($post_id, 'video_thumb_url', get_post_meta($post_id, 'video_thumb_url', true));
-    
 }
 
-function get_video_thumb($video_id, $video_type) {
-    if ($video_id == '') return '';
-
-    switch($video_type) {
-        case 'vimeo':
-            //forming API url
-            $url = "http://vimeo.com/api/v2/video/{$video_id}.json";
-            // enable curl
-            ini_set("allow_url_fopen",true);
-
-            //curl request
-            $curl = curl_init();
-            curl_setopt($curl, CURLOPT_URL, $url);
-            curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-            $curlData = curl_exec($curl);
-            curl_close($curl);
-            
-            //decoding json structure into array
-            $arr = current(json_decode($curlData, true));
-            
-            $thumbnail = $arr["thumbnail_large"];
-            break;
-        case 'youtube':
-            $thumbnail =  "http://i.ytimg.com/vi/{$video_id}/mqdefault.jpg";
-            break;
-    }
-
-    return $thumbnail;
-}
-
-function render_video($video_id, $video_type, $return = false) {
+function render_video($video_id, $video_type, $return_rendered_video = false) {
     
     switch ($video_type) {
         case 'vimeo':
@@ -158,7 +108,7 @@ function render_video($video_id, $video_type, $return = false) {
             break;  
     }
     if ($video_id == '') return; // validate video, if no video id has been sent, don't render video
-    if ($return)
+    if ($return_rendered_video)
         return $embed;
     else
         echo $embed;
