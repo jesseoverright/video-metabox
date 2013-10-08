d6ab1024dfafa9115bac1f890098a1564d210be9
Jesse Overright <jesseoverright@gmail.com>
2013-10-08 15:54:03 -0500

    Added responsive video and refactored some plugin code

--- video-metabox.php
+++ video-metabox.php
@@ -25,40 +25,45 @@
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
-add_action("admin_init", "add_video_metabox");
-add_action("save_post", "save_video_metabox");
+add_action('admin_init', 'add_video_metabox');
+add_action('save_post', 'save_video_metabox');
 
+add_action('init', 'add_video_metabox_css');
 
 function add_video_metabox () {
-         add_meta_box("video-meta", "Video", "video_meta_box", "post", "normal", "high");
-    }
+     add_meta_box('video-meta', 'Video', 'video_meta_box', 'post', 'normal', 'high');
+}
+
+function add_video_metabox_css() {
+    wp_enqueue_style( 'video-metabox-css', plugins_url('video-metabox.css', __FILE__) );
+}    
 
 function video_meta_box () {
-         global $post;
+     global $post;
 
-        // Verify
-        echo'<input type="hidden" name="video_noncename" id="video_noncename" value="'.wp_create_nonce( plugin_basename(__FILE__) ).'" />';
-        
-        $video_id = get_post_meta($post->ID, 'video_id', true);
-        $video_type = get_post_meta($post->ID, 'video_type', true);
-        $video_thumb_url = get_post_meta($post->ID, 'video_thumb_url', true);
-        
-        if ($video_thumb_url != "")
-        {
-            echo '<img src="'.$video_thumb_url.'" style="margin: 5px 0 10px;" /><br />';    
-        }
-        ?>
-        <label for="video_type" style="margin-right: 5px;">Video Type:</label>
-        <select name="video_type">
-            <option value="youtube" <?php if ($video_type == "youtube") echo ' selected';?>>YouTube</option>
-            <option value="vimeo" <?php if ($video_type == "vimeo") echo ' selected';?>>Vimeo</option>
-        </select>
-        <label for="video_id" style="margin-right: 18px;margin-left:8px;">Video ID:</label>
-        <input type="text" name="video_id" value="<?php echo $video_id; ?>" size="20" /><br />
-        <p>Example Video IDs: www.youtube.com/watch?v=<b>hfbwoCqLgJo</b>, http://vimeo.com/<b>29491315</b></p>
-
-        <?php
+    // Verify
+    echo'<input type="hidden" name="video_noncename" id="video_noncename" value="'.wp_create_nonce( plugin_basename(__FILE__) ).'" />';
+    
+    $video_id = get_post_meta($post->ID, 'video_id', true);
+    $video_type = get_post_meta($post->ID, 'video_type', true);
+    $video_thumb_url = get_post_meta($post->ID, 'video_thumb_url', true);
+    
+    if ($video_thumb_url != "")
+    {
+        echo '<img src="'.$video_thumb_url.'" style="margin: 5px 0 10px;" /><br />';    
     }
+    ?>
+    <label for="video_type" style="margin-right: 5px;">Video Type:</label>
+    <select name="video_type">
+        <option value="youtube" <?php if ($video_type == "youtube") echo ' selected';?>>YouTube</option>
+        <option value="vimeo" <?php if ($video_type == "vimeo") echo ' selected';?>>Vimeo</option>
+    </select>
+    <label for="video_id" style="margin-right: 18px;margin-left:8px;">Video ID:</label>
+    <input type="text" name="video_id" value="<?php echo $video_id; ?>" size="20" /><br />
+    <p>Example Video IDs: www.youtube.com/watch?v=<b>hfbwoCqLgJo</b>, http://vimeo.com/<b>29491315</b></p>
+
+    <?php
+}
     
 function save_video_metabox( $post_id ) {
     global $post;
@@ -96,80 +101,64 @@ function save_video_metabox( $post_id ) {
             
     // depending on the video type, get image location
     ini_set("allow_url_fopen",true);
-    // VIMEO
-    if ($video_type == "vimeo") {
-        if ($video_thumb_url == "" && $video_id != "") {
-            $video_thumb_url = get_vimeo_thumb($video_id);
-        }
-    }
-    // YOUTUBE
-    elseif ($video_type == "youtube") {
-        if ($video_thumb_url == "" && $video_id != "") {
-            $video_thumb_url = get_youtube_thumb($video_id);
-        }
-    }
+    $video_thumb_url = get_video_thumb($video_id, $video_type);
+
     // if no video, remove thumbnail
-    if ($video_id == "")
+    if ($video_id == '')
     {
-        $video_thumb_url = "";
+        $video_thumb_url = '';
     }
     
-    if(get_post_meta($post_id, 'video_thumb_url') == "")
+    if(get_post_meta($post_id, 'video_thumb_url') == '')
     add_post_meta($post_id, 'video_thumb_url', $video_thumb_url, true);
     elseif($video_thumb_url != get_post_meta($post_id, 'video_thumb_url', true))
     update_post_meta($post_id, 'video_thumb_url', $video_thumb_url);
-    elseif($video_thumb_url == "")
+    elseif($video_thumb_url == '')
     delete_post_meta($post_id, 'video_thumb_url', get_post_meta($post_id, 'video_thumb_url', true));
     
 }
-// additional functions for video metabox 
-function get_vimeo_thumb($vimeo)
-{
-    if ($vimeo == "")
-        return "";
-    $id = $vimeo;
-    //forming API url
-    $url = "http://vimeo.com/api/v2/video/".$id.".json";
-    //curl request
-    $curl = curl_init();
-    curl_setopt($curl, CURLOPT_URL, $url);
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-    $curlData = curl_exec($curl);
-    curl_close($curl);
-    
-    //decoding json structure into array
-    $arr = current(json_decode($curlData, true));
-    
-    /*  Vimeo Thumbnail Sizes:
-     thumbnail_small => 100x75
-     thumbnail_medium => 200x150
-     thumbnail_large => 640x360
-    */
-    return $arr["thumbnail_large"];
 
+function get_video_thumb($video_id, $video_type) {
+    if ($video_id == '') return '';
+
+    switch($video_type) {
+        case 'vimeo':
+            //forming API url
+            $url = "http://vimeo.com/api/v2/video/{$video_id}.json";
+            //curl request
+            $curl = curl_init();
+            curl_setopt($curl, CURLOPT_URL, $url);
+            curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+            $curlData = curl_exec($curl);
+            curl_close($curl);
+            
+            //decoding json structure into array
+            $arr = current(json_decode($curlData, true));
+            
+            $thumbnail = $arr["thumbnail_large"];
+            break;
+        case 'youtube':
+            $thumbnail =  "http://i.ytimg.com/vi/{$video_id}/mqdefault.jpg";
+            break;
+    }
+
+    return $thumbnail;
 }
 
-function get_youtube_thumb($video_id)
-{
-    return "http://i.ytimg.com/vi/".$video_id."/mqdefault.jpg";
-    
-}
-
-function render_video($video_id, $video_type, $video_width = 900, $return = false) {
-    $video_height = round($video_width / 16 * 9);
+function render_video($video_id, $video_type, $return = false) {
     
     switch ($video_type) {
-        case "vimeo":
-            $embed = '<iframe src="http://player.vimeo.com/video/'.$video_id.'?title=0&byline=0&portrait=0&color=ffffff" width="'.$video_width.'" height="'.$video_height.'" frameborder="0" webkitAllowFullScreen allowFullScreen></iframe>';
+        case 'vimeo':
+            $embed = "<div class=\"video-metabox\"><iframe src=\"http://player.vimeo.com/video/{$video_id}?title=0&byline=0&portrait=0&color=ffffff\" frameborder=\"0\" webkitAllowFullScreen allowFullScreen></iframe></div>";
             break;
-        case "youtube":
-            $embed = '<iframe width="'.$video_width.'" height="'.$video_height.'" src="http://www.youtube.com/embed/'.$video_id.'/?modestbranding=1&rel=0&showinfo=0" frameborder="0" allowfullscreen></iframe>';
+        case 'youtube':
+            $embed = "<div class=\"video-metabox\"><iframe src=\"http://www.youtube.com/embed/{$video_id}/?modestbranding=1&rel=0&showinfo=0\" frameborder=\"0\" allowfullscreen></iframe></div>";
             break;
         default:
-            $embed = "";
+            $embed = '';
             break;  
     }
-    if ($video_id == "") return; // validate video, if no video id has been sent, don't render video
+    if ($video_id == '') return; // validate video, if no video id has been sent, don't render video
     if ($return)
         return $embed;
     else
@@ -181,7 +170,7 @@ add_filter( 'the_content' , 'video_metabox_content_filter');
 
 function video_metabox_content_filter( $content ) {
     global $post;
-    if (get_post_meta($post->ID,'video_id',true) != "") {
+    if (get_post_meta($post->ID,'video_id',true) != '') {
         $content = render_video(get_post_meta($post->ID,'video_id',true),get_post_meta($post->ID,'video_type',true),640) . $content;
     }
 
